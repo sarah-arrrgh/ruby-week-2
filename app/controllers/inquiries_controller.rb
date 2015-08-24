4361ffe3e5eb41c2ae956da519f8e1c3a2a6d53c
Sarah Rogers <email": null>
2015-08-24 19:42:25 +1200

    Slice five-pass eight 'Sending the email' completed

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,10 +1,17 @@
 class InquiriesController < ApplicationController
-
   def new
   end
 
   def create
-    flash.notice = "Thank you for sending us a message."
+    inquiry = Inquiry.new
+    inquiry.name = params[:inquiry][:name]
+    inquiry.email = params[:inquiry][:email]
+    inquiry.subject = params[:inquiry][:subject]
+    inquiry.message = params[:inquiry][:message]
+
+    InquiryMailer.inquiry_email(inquiry).deliver_now
+
+    flash[:notice] = "Thank you for sending us a message."
     redirect_to root_url
   end
 end
