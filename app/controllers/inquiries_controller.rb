cf1a1f9c41887b5f791dc9e6b301b0bd0c3aeda9
Sarah Rogers <email": null>
2015-08-25 19:16:38 +1200

    Slice six-pass two 'Validation' completed

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,18 +1,23 @@
 class InquiriesController < ApplicationController
   def new
+    @inquiry = Inquiry.new
   end
 
-  def create
-    inquiry = Inquiry.create(inquiry_params)
+def create
+  @inquiry = Inquiry.create(inquiry_params)
 
-    InquiryMailer.inquiry_email(inquiry).deliver_now
+  if (@inquiry.valid?)
+    InquiryMailer.inquiry_email(@inquiry).deliver_now
 
-    flash[:notice] = "Thank you for sending us a message, #{inquiry.name}"
+    flash[:notice] = "Thank you for sending us a message, #{@inquiry.name}"
     redirect_to root_url
+  else
+    flash.now.alert = "There is a problem with your form. Please check it."
+    render :new
   end
+end
 
-  private
-
+private
   def inquiry_params
     params.require(:inquiry).permit( :name, :email, :subject, :message )
   end
--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -3,6 +3,7 @@ class Inquiry
 
   field :name, type: String
   field :email, type: String
+  validates :email, presence: true
   field :subject, type: String
   field :message, type: String
 end
