1033451bfe2c2090c98fadd6db845146b366e4dc
Sarah Rogers <email": null>
2015-08-25 19:37:16 +1200

    Slice six-pass four 'Feature test?' completed

--- spec/features/inquiries_spec.rb
+++ spec/features/inquiries_spec.rb
@@ -12,7 +12,10 @@ RSpec.feature "Inquiries", type: :feature do
   scenario 'user can complete and submit the form' do
     visit contact_path
 
-    fill_in 'Message', with: "This is my message"
+    fill_in 'Name', with: "Joe Mama"
+    fill_in 'Email', with: "joe@mama.com"
+    fill_in 'Subject', with: "More beer!"
+    fill_in 'Message', with: "And let's get some donuts in here!"
 
     click_button 'Send message'
 
